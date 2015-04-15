ea1b4f06302ff0dafccb15ab41874f44b9fb3ca8
Pedro Belo <pedro@heroku.com>
2015-04-14 18:13:48 -0700

    actually really delegate to the async storage

--- application/storage/local.js
+++ application/storage/local.js
@@ -6,11 +6,21 @@ var { AsyncStorage } = React;
 module.exports = {
   getTodos: function() {
     return new Promise((resolve, reject) => {
-      var todos = [
-        {txt: 'Learn react native', complete:false},
-        {txt: 'Make a to-do app', complete:true}
-      ];
-      resolve(todos);
+      AsyncStorage.getItem('todos')
+        .then((value) => {
+          if (value !== null) {
+            resolve(value);
+          }
+          else {
+            var todos = [
+              {txt: 'Learn react native', complete:false},
+              {txt: 'Make a to-do app', complete:true}
+            ];
+            resolve(todos);
+          }
+        })
+        .catch(reject)
+        .done();
     });
   }
 };
